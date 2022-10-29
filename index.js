c4ee057c0554c57424559e57dcb4106eb250b4f1
jorbencas <jorbencas@gmail.com>
2022-10-29 18:56:51 +0200

    Juego funcionando con mejoras

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const $ = (selector) => document.querySelector(selector);
 const $id = (selector) => document.getElementById(selector);
-const tableroHTML = $id("board");
 const win = 3;
 let players = {
   1: { icon: "cross", state: ``, name: "", moves: 0, numWins: 0, numLost: 0 },
@@ -14,41 +13,86 @@ let playState = {
 };
 
 function changeMultiplayer(e) {
-  if(playState.numPlays === 3){
-    dispatch('CHANGE_MULTIPLAYER',parseInt(e.currentTarget.value));
-    if (playState.multiplayer == 2) {
-      $id("options").innerHTML = `
+  if (playState.numPlays === 3) {
+    dispatch("CHANGE_MULTIPLAYER", parseInt(e.currentTarget.value));
+    changeViewMultiPlayer();
+  }
+}
+
+function changeNameMultiPlayer(namePlayer = "", player) {
+  dispatch("SET_NAME_PLAYER", namePlayer, player);
+  $("div[id^='" + player + "'] .message").innerText = namePlayer;
+}
+
+function changeViewMultiPlayer() {
+  if (playState.multiplayer == 2) {
+    $id("options").innerHTML = `
       <input type="text" onchange='changeNameMultiPlayer(event.currentTarget.value,1)' id="nombreJugador" value=''>
         <label for="nombreJugador">Nombre del jugador 1</label>
         
         <input type="text" onchange='changeNameMultiPlayer(event.currentTarget.value,2)' id="nombreJugador" value=''>
         <label for="nombreJugador">Nombre del jugador 3</label>
         `;
-    } else {
-      $id("options").innerHTML = "";
-      changeNameMultiPlayer('',1);
-      changeNameMultiPlayer('',2);
+  } else {
+    $id("options").innerHTML = "";
+    changeNameMultiPlayer("", 1);
+    changeNameMultiPlayer("", 2);
+  }
+}
+
+function resetPlayState() {
+  $("div[id^='1'] .message").innerText = "";
+  $("div[id^='2'] .message").innerText = "";
+  $("div[id^='1']").classList.remove(players[1].state);
+  $("div[id^='2']").classList.remove(players[2].state);
+  dispatch("CHANGE_TURN", "", 2);
+  resetBoard();
+  dispatch("DESINCREMENT_NUM_PLAYS");
+  $id(playState.activePlayer).classList.add("active");
+  if (playState.numPlays == 0) {
+    changeViewMultiPlayer();
+    dispatch("RESET_NUM_PLAYS");
+    $id("multiplayer").setAttribute("visibility", "visible");
+    if (playState.multiplayer == 2) {
+      $id("options").innerHTML =
+        "el jugador" +
+        players[1].name +
+        " ha hecho " +
+        players[1].moves +
+        " movimientos y ha ganado " +
+        players[1].numWins +
+        " mientras que ha perdido " +
+        players[1].numLost +
+        " partidas " +
+        " y el jugador" +
+        players[2].name +
+        " ha hecho " +
+        players[2].moves +
+        " movimientos y ha ganado" +
+        players[2].numWins +
+        " mientras que ha perdido " +
+        players[2].numLost +
+        " partidas ";
     }
+    dispatch("RESET_PLAYSTATE");
+  } else {
+    $id("options").innerHTML = playState.numPlays + "partidas";
   }
 }
 
-function changeNameMultiPlayer(namePlayer = "", player) {
-  dispatch('SET_NAME_PLAYER', namePlayer,player);
-}
-
-function nextPlay(){
+function nextPlay() {
   let position = {
-    cell:0,
-    row:0
-  }
-  playState.board.forEach((e,i) => {
-    e.forEach((c,j) => {
-      if(c > 0 && position.row == 0 && position.cell == 0){
+    cell: 0,
+    row: 0,
+  };
+  playState.board.forEach((e, i) => {
+    e.forEach((c, j) => {
+      if (c == 0 && position.row == 0 && position.cell == 0) {
         position.row = i;
         position.cell = j;
       }
-    })
-  })
+    });
+  });
   return position;
 }
 /*
@@ -87,11 +131,15 @@ function isEndByHorizontal(x) {
 
 function isEndByDiagonalFalling() {
   let end = false;
-  if (playState.board.filter((_, j) => playState.board[j][j] == 1).length == win) {
+  if (
+    playState.board.filter((_, j) => playState.board[j][j] == 1).length == win
+  ) {
     end = true;
     players[1].state = "ganas";
     players[2].state = "pierdes";
-  } else if (playState.board.filter((_, j) => playState.board[j][j] == 2).length == win) {
+  } else if (
+    playState.board.filter((_, j) => playState.board[j][j] == 2).length == win
+  ) {
     end = true;
     players[1].state = "pierdes";
     players[2].state = "ganas";
@@ -102,11 +150,17 @@ function isEndByDiagonalFalling() {
 function isEndByDiagonalUpward() {
   let end = false;
   let last = playState.board.length - 1;
-  if (playState.board.filter((_, i) => playState.board[i][last - i] == 1).length == win) {
+  if (
+    playState.board.filter((_, i) => playState.board[i][last - i] == 1)
+      .length == win
+  ) {
     end = true;
     players[1].state = "ganas";
     players[2].state = "pierdes";
-  } else if (playState.board.filter((_, i) => playState.board[i][last - i] == 2).length == win) {
+  } else if (
+    playState.board.filter((_, i) => playState.board[i][last - i] == 2)
+      .length == win
+  ) {
     end = true;
     players[1].state = "pierdes";
     players[2].state = "ganas";
@@ -116,7 +170,10 @@ function isEndByDiagonalUpward() {
 
 function isEndByTie() {
   let end = false;
-  if (playState.board.filter((e) => e.filter((f) => f > 0).length == win).length == win) {
+  if (
+    playState.board.filter((e) => e.filter((f) => f > 0).length == win)
+      .length == win
+  ) {
     end = true;
     players[1].state = "empate";
     players[2].state = "empate";
@@ -125,8 +182,9 @@ function isEndByTie() {
 }
 
 function resetBoard() {
-  dispatch('CREATE_BOARD', {rows:win, cells:win});
-  tableroHTML.innerHTML = playState.board.map((e, y) => {
+  dispatch("CREATE_BOARD", { rows: win, cells: win });
+  $id("board").innerHTML = playState.board
+    .map((e, y) => {
       return (
         `<div class='row'>` +
         e
@@ -142,10 +200,23 @@ function resetBoard() {
 }
 
 function play(x, y) {
-  if (playState.board[y][x] == 0 && playState.numPlays > 0) {
-    $id(
-      y + "-" + x
-    ).innerHTML = `<img src="${players[playState.activePlayer].icon}.svg" alt="Jugador ${activePlayer}">`;
+  if (
+    playState.multiplayer == 2 &&
+    players[1].name.length == 0 &&
+    players[2].name.length == 0
+  ) {
+    return;
+  }
+
+  if (playState.numPlays == 3) {
+    $id("multiplayer").setAttribute("visibility", "hidden");
+    $id("options").innerHTML = "";
+  }
+
+  if (playState.board[y][x] == 0) {
+    $id(y + "-" + x).innerHTML = `<img src="${
+      players[playState.activePlayer].icon
+    }.svg" alt="Jugador ${playState.activePlayer}">`;
     playState.board[y][x] = playState.activePlayer;
 
     if (
@@ -155,10 +226,10 @@ function play(x, y) {
       isEndByDiagonalUpward() ||
       isEndByTie()
     ) {
-      if(playState.multiplayer == 2){
-        dispatch('ADD_NUM_WINS');
+      if (playState.multiplayer == 2) {
+        dispatch("ADD_NUM_WINS");
         let numPlayer = players[1].state == "ganas" ? 2 : 1;
-        dispatch('ADD_NUM_LOSTS','',numPlayer);
+        dispatch("ADD_NUM_LOSTS", "", numPlayer);
       }
       $id(playState.activePlayer).classList.remove("active");
       $("div[id^='1'] .message").innerText = players[1].state.toUpperCase();
@@ -166,24 +237,15 @@ function play(x, y) {
       $("div[id^='1']").classList.add(players[1].state);
       $("div[id^='2']").classList.add(players[2].state);
       setTimeout(() => {
-        $("div[id^='1'] .message").innerText = "";
-        $("div[id^='2'] .message").innerText = "";
-        $("div[id^='1']").classList.remove(players[1].state);
-        $("div[id^='2']").classList.remove(players[2].state);
-        dispatch('CHANGE_TURN',2);
-        resetBoard();
-        dispatch('RESET_PLAYSTATE');
-        $id(playState.activePlayer).classList.add("active");
+        resetPlayState();
       }, 2000);
     } else {
-      $("div[id^='" + playState.activePlayer + "'] .message").innerText =
-        players[playState.activePlayer].name;
       $id(playState.activePlayer).classList.remove("active");
-      dispatch('CHANGE_TURN');
+      dispatch("CHANGE_TURN");
       if (playState.multiplayer == 2) {
-        dispatch('ADD_NUM_MOVES');
-      } else if(playState.multiplayer == 1 && playState.activePlayer == 2){
-        const {row, cell} = nextPlay();
+        dispatch("ADD_NUM_MOVES");
+      } else if (playState.multiplayer == 1 && playState.activePlayer == 2) {
+        const { row, cell } = nextPlay();
         play(row, cell);
       }
       $id(playState.activePlayer).classList.add("active");
@@ -191,35 +253,48 @@ function play(x, y) {
   }
 }
 
-function dispatch(action, value = '', player = playState.activePlayer){
-  if(action == 'ADD_NUM_WINS'){
+function dispatch(action, value = "", player = playState.activePlayer) {
+  if (action == "ADD_NUM_WINS") {
     players[player].numWins++;
-  } else if(action == 'ADD_NUM_LOSTS'){
+  } else if (action == "ADD_NUM_LOSTS") {
     players[player].numLost++;
-  } else if(action == 'ADD_NUM_MOVES'){
+  } else if (action == "ADD_NUM_MOVES") {
     players[player].moves++;
-  } else if(action == 'DESINCREMENT_NUM_PLAYS'){
+  } else if (action == "DESINCREMENT_NUM_PLAYS") {
     playState.numPlays--;
-  } else if(action == 'SET_NAME_PLAYER'){
+  } else if (action == "SET_NAME_PLAYER") {
     players[player].name = value;
-  } else if(action == 'RESET_PLAYSTATE'){
+  } else if (action == "RESET_PLAYSTATE") {
     players = {
-      1: { icon: "cross", state: ``, name: "", moves: 0, numWins: 0, numLost: 0 },
-      2: { icon: "circle", state: ``, name: "", moves: 0, numWins: 0, numLost: 0 },
-    }
-  } else if(action == 'CHANGE_MULTIPLAYER'){
+      1: {
+        icon: "cross",
+        state: ``,
+        name: "",
+        moves: 0,
+        numWins: 0,
+        numLost: 0,
+      },
+      2: {
+        icon: "circle",
+        state: ``,
+        name: "",
+        moves: 0,
+        numWins: 0,
+        numLost: 0,
+      },
+    };
+  } else if (action == "CHANGE_MULTIPLAYER") {
     playState.multiplayer = value;
-  } else if(action == 'CHANGE_TURN'){
-    playState.activePlayer = value === 1 ? 2 : 1;
-  } else if (action == 'CREATE_BOARD'){
-    const {rows, cells} = value;
+  } else if (action == "CHANGE_TURN") {
+    playState.activePlayer = player === 1 ? 2 : 1;
+  } else if (action == "CREATE_BOARD") {
+    const { rows, cells } = value;
     playState.board = Array.from({ length: rows }, (_, i) =>
-    Array.from({ length: cells }, (_, i) => "")
-  );
+      Array.from({ length: cells }, (_, i) => "")
+    );
+  } else if (action == "RESET_NUM_PLAYS") {
+    playState.numPlays = 3;
   }
 }
-
-
-
 
 resetBoard();
